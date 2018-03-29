627bee70876cd3457f89289713e17e47251a6455
Ildar Shaynurov <shaynurov@gmail.com>
2018-03-29 16:56:52 +0300

    make simple proxy server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,33 @@
-console.log('test');
+import * as http from 'http';
+
+// const DNS_PORT   = 23400;
+const HTTP_PORT  = 23401;
+// const HTTPS_PORT = 23402;
+
+const httpServer = http.createServer();
+
+httpServer.on('request', (request, response) => {
+  console.log('http request.headers: ', request.headers);
+
+  const requestOptions = {
+    host:    'localhost',
+    port:    3000,
+    path:    request.url,
+    method:  request.method,
+    headers: request.headers,
+  };
+  const proxyRequest = http.request(requestOptions, (proxyResponse) => {
+    proxyResponse.pipe(response);
+    proxyResponse.on('data', () => {
+    });
+    response.writeHead(proxyResponse.statusCode, proxyResponse.headers);
+  });
+  request.pipe(proxyRequest);
+  // response.writeHead(200, { 'Content-Type':'text/plain' });
+  // response.write('Hello world');
+  // response.end();
+});
+
+httpServer.listen(HTTP_PORT, () => {
+  console.log(`Node server created at port ${HTTP_PORT}`);
+});
