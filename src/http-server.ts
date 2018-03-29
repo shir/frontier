8b75fa0f769f4429b12f2c9a6a944182aef6c61f
Ildar Shaynurov <shaynurov@gmail.com>
2018-03-29 17:08:15 +0300

    move http server to separate file

---
+++ src/http-server.ts
@@ -0,0 +1,43 @@
+import * as http from 'http';
+
+// const DNS_PORT   = 23400;
+const HTTP_PORT  = 23401;
+// const HTTPS_PORT = 23402;
+
+function createHttpServer() {
+  const httpServer = http.createServer();
+
+  httpServer.on('request', (request, response) => {
+    console.log('http request.headers: ', request.headers);
+
+    const requestOptions = {
+      host:    'localhost',
+      port:    3000,
+      path:    request.url,
+      method:  request.method,
+      headers: request.headers,
+    };
+    const proxyRequest = http.request(requestOptions, (proxyResponse) => {
+      proxyResponse.pipe(response);
+      proxyResponse.on('data', () => {
+      });
+      response.writeHead(proxyResponse.statusCode, proxyResponse.headers);
+    });
+    request.pipe(proxyRequest);
+    // response.writeHead(200, { 'Content-Type':'text/plain' });
+    // response.write('Hello world');
+    // response.end();
+  });
+
+  httpServer.on('close', () => {
+    console.log('Stopping HTTP server');
+  });
+
+  httpServer.listen(HTTP_PORT, () => {
+    console.log(`Node server created at port ${HTTP_PORT}`);
+  });
+
+  return httpServer;
+}
+
+export { createHttpServer };
--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,7 @@
-import * as http from 'http';
+import { createHttpServer } from './http-server';
 
-// const DNS_PORT   = 23400;
-const HTTP_PORT  = 23401;
-// const HTTPS_PORT = 23402;
+const httpServer = createHttpServer();
 
-const httpServer = http.createServer();
-
-httpServer.on('request', (request, response) => {
-  console.log('http request.headers: ', request.headers);
-
-  const requestOptions = {
-    host:    'localhost',
-    port:    3000,
-    path:    request.url,
-    method:  request.method,
-    headers: request.headers,
-  };
-  const proxyRequest = http.request(requestOptions, (proxyResponse) => {
-    proxyResponse.pipe(response);
-    proxyResponse.on('data', () => {
-    });
-    response.writeHead(proxyResponse.statusCode, proxyResponse.headers);
-  });
-  request.pipe(proxyRequest);
-  // response.writeHead(200, { 'Content-Type':'text/plain' });
-  // response.write('Hello world');
-  // response.end();
-});
-
-httpServer.listen(HTTP_PORT, () => {
-  console.log(`Node server created at port ${HTTP_PORT}`);
+process.on('SIGINT', () => {
+  httpServer.close();
 });
