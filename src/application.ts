40a7f6784dca42e7a16ac56272a5237257212b6d
Ildar Shaynurov <shaynurov@gmail.com>
2018-03-30 11:52:08 +0300

    feat(Application): add directory parameter

--- src/application.ts
+++ src/application.ts
@@ -4,21 +4,24 @@ interface ApplicationParams {
   name:      string;
   hostname?: string;
   port:      number;
+  directory: string;
   runCmd:    string;
 }
 
 class Application {
-  name:     string;
-  hostname: string;
-  port:     number;
-  runCmd:   string;
-  pid:      number | null = null;
+  name:      string;
+  hostname:  string;
+  port:      number;
+  directory: string;
+  runCmd:    string;
+  pid:       number | null = null;
 
   constructor(params: ApplicationParams) {
-    this.name     = params.name;
-    this.hostname = params.hostname || `${params.name}.test`;
-    this.port     = params.port;
-    this.runCmd   = params.runCmd;
+    this.name      = params.name;
+    this.hostname  = params.hostname || `${params.name}.test`;
+    this.port      = params.port;
+    this.directory = params.directory;
+    this.runCmd    = params.runCmd;
   }
 
   run = () => {
