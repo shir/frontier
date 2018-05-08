522f9370f342c44d64dd6d8ac2033d4035128c1e
Ildar Shaynurov <shaynurov@gmail.com>
2018-05-08 15:11:17 +0300

    refact(Install): create base class for installers and move template rendering there

--- src/installer/firewall.ts
+++ src/installer/firewall.ts
@@ -1,44 +1,26 @@
-import * as path from 'path';
-import * as fs from 'fs';
-
-import logger from '../logger';
 import config from '../config';
 
-import * as ejs from 'ejs';
+import Base from './base';
 
-class FirewallInstaller {
+class FirewallInstaller extends Base {
   installPfAnchor = () => {
-    logger.info(`Write ${config.pfAnchorFilePath}`);
-    ejs.renderFile(
-      path.join(__dirname, '..', 'templates', 'pf-anchor.ejs'),
+    this.renderTemplate(
+      'pf-anchor',
+      config.pfAnchorFilePath,
       {
         httpPort:  config.httpServerPort,
         httpsPort: config.httpsServerPort,
-      },
-      (err, result) => {
-        if (err) {
-          logger.error(`Error on rendering PF anchor template: ${err.message}`);
-          return;
-        }
-        fs.writeFileSync(config.pfAnchorFilePath, result, { mode: '0644' });
       },
     );
   }
 
   installPfConf = () => {
-    logger.info(`Write ${config.pfConfFilePath}`);
-    ejs.renderFile(
-      path.join(__dirname, '..', 'templates', 'pf.conf.ejs'),
+    this.renderTemplate(
+      'pf.conf',
+      config.pfConfFilePath,
       {
         anchor:         config.pfAnchorName,
         anchorFilePath: config.pfAnchorFilePath,
-      },
-      (err, result) => {
-        if (err) {
-          logger.error(`Error on rendering PF config template: ${err.message}`);
-          return;
-        }
-        fs.writeFileSync(config.pfConfFilePath, result, { mode: '0644' });
       },
     );
   }
--- src/installer/resolver.ts
+++ src/installer/resolver.ts
@@ -1,24 +1,13 @@
-import * as path from 'path';
-import * as fs from 'fs';
-
-import logger from '../logger';
 import config from '../config';
 
-import * as ejs from 'ejs';
+import Base from './base';
 
-class ResolverInstaller {
+class ResolverInstaller extends Base {
   perform = () => {
-    logger.info(`Write ${config.resolverFilePath}`);
-    ejs.renderFile(
-      path.join(__dirname, '..', 'templates', 'resolver.ejs'),
+    this.renderTemplate(
+      'resolver',
+      config.resolverFilePath,
       { port: config.dnsServerPort },
-      (err, result) => {
-        if (err) {
-          logger.error(`Error on rendering resolver template: ${err.message}`);
-          return;
-        }
-        fs.writeFileSync(config.resolverFilePath, result, { mode: '0644' });
-      },
     );
   }
 }
